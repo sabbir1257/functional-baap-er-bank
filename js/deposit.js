365795ac42f19a6e259ac0216eaf0e407e76a3ed
sabbir1257 <masabbir1257@gmail.com>
2023-02-15 02:59:20 +0600

    push

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,34 @@
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const
+    inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString)
+    inputField.value = '';
+    return inputFieldValue; 
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
     /*
     1.get the element by id 
     2. get the value from the element
     3. convert string value to a number
     */ 
+
+    const newDepositAmount = getInputFieldValueById('deposit-input');
+
+    /*
+    1. get previous depasit total by id
+    */ 
+   const previousDepositTotal = getElementValueById('deposite-total');
+
+    // calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    
 })
